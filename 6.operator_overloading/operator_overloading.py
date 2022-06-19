b60dc5a2368f4a23b82722e06981e41fd3b6fd6b
Sasha Ajin <sashaajin@icloud.com>
2022-06-19 13:38:41 +0300

    edit 'operator overloading'

--- 6.operator_overloading/operator_overloading.py
+++ 6.operator_overloading/operator_overloading.py
@@ -69,7 +69,8 @@ We can overload comparison operators as well.
 Suppose we wanted to implement the less than symbol < symbol in our Point class.
 
 Let us compare the magnitude of these points from the origin and return the 
-result for this purpose. It can be implemented as follows."""
+result for this purpose. It can be implemented as follows.
+"""
 class Point:
     def __init__(self, x=0, y=0):
         self.x = x
